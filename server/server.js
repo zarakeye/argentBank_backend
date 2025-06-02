43a901ec1baca7d4e994d88fc0088c5126bdedff
Stéphane Bazzé <stephane.bazze@outlook.fr>
2025-06-02 20:42:00 +0200

    adapt swagger yaml

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const dotEnv = require('dotenv')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
-const swaggerDocs = yaml.load('./swagger.yaml')
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const swaggerDocs = NODE_ENV === 'production' ? yaml.load('./swagger_prod.yaml') : yaml.load('./swagger.yaml')
 const dbConnection = require('./database/connection')
 
 const cookieParser = require('cookie-parser')
