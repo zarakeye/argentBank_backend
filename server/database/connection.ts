d814c33f07f5f9ec3a9f413f2531cf5c401c2e3f
Stéphane Bazzé <stephane.bazze@outlook.fr>
2025-06-02 19:40:55 +0200

    .env ready

---
+++ server/database/connection.ts
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+require('dotenv').config()
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+
+const DATABASE_URL: string | undefined = NODE_ENV === 'production' ? process.env.DATABASE_URL_PROD : process.env.DATABASE_URL_DEV// || 'mongodb://localhost/argentBankDB'
+
+if (!DATABASE_URL) {
+  console.error('❌ DATABASE_URL is undefined. Check your .env file and NODE_ENV setting.');
+  process.exit(1);
+}
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
+    console.log('Database successfully connected')
+  } catch (error) {
+    console.error(`Database Connectivity Error: ${error}`)
+    throw new Error(error)
+  }
+}
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('./swagger.yaml')
-const dbConnection = require('./database/connection')
+const dbConnection = require('./database/connection.ts')
 
 const cookieParser = require('cookie-parser')
 
