427c0544d5b8665ed5a62a930cff3484000e53a7
Stéphane Bazzé <stephane.bazze@outlook.fr>
2025-06-05 17:49:11 +0200

    Frontend implemented with ViteJs must be deplyed on Netlify

--- server/server.js
+++ server/server.js
@@ -17,15 +17,11 @@ const app = express()
 // Connect to the database
 dbConnection()
 
-console.log('NODE_ENV:', process.env.NODE_ENV)
 // Handle CORS issues
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'argent-bank-gamma.vercel.app'/*'https://argentblanknetlify.netlify.app'*/ : 'http://localhost:5173'/*'http://localhost:8888'*/,
+  origin: process.env.NODE_ENV === 'production' ? 'https://argentblanknetlify.netlify.app' : 'http://localhost:8888',
   credentials: true
 }))
-console.log('CORS origin:', process.env.NODE_ENV === 'production'
-  ? 'https://argentblanknetlify.netlify.app'
-  : 'http://localhost:8888');
 // Request payload middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
